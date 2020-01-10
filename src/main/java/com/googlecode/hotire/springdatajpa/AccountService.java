a0916ee97aeb5ff1e0b48f4ac559d2dfb9c2c636
user <gngh0101@gmail.com>
2020-01-11 01:23:37 +0900

    Add comment for entity manager

--- src/main/java/com/googlecode/hotire/springdatajpa/AccountService.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/AccountService.java
@@ -1,14 +1,15 @@
 package com.googlecode.hotire.springdatajpa;
 
-import static javax.persistence.PersistenceContextType.EXTENDED;
-
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * EntityManager Life Cycle Test
+ *
+ */
 //@Transactional
 @Service
 public class AccountService {
@@ -25,10 +26,6 @@ public class AccountService {
     account = entityManager.find(Account.class, 1L);
     account = entityManager.find(Account.class, 1L);
 
-
-
-    System.out.println(account);
-    System.out.println(account);
 //    if (1==1)throw new RuntimeException("");
 
     System.out.println("");
