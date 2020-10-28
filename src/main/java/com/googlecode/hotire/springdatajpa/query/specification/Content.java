4af4e32f9fce9ecf724f73169f39ed3ffe45fe2e
hotire <gngh0101@gmail.com>
2020-10-29 00:18:45 +0900

    Add Content entity

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/query/specification/Content.java
@@ -0,0 +1,11 @@
+package com.googlecode.hotire.springdatajpa.query.specification;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Content {
+    @Id @GeneratedValue
+    private Long id;
+}
