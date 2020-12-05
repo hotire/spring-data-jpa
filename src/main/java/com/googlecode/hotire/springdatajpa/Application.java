ce3a6cafc17b9fdac1b341e0df1223783efb96bd
hotire <gngh0101@gmail.com>
2020-12-05 23:19:35 +0900

    NO-ISSUE Use dateTimeProviderRef instaed of Audit config

--- src/main/java/com/googlecode/hotire/springdatajpa/Application.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/Application.java
@@ -1,5 +1,7 @@
 package com.googlecode.hotire.springdatajpa;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.InitializingBean;
@@ -7,14 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
 
-import com.googlecode.hotire.springdatajpa.audit.AuditConfig;
-
-@Import(AuditConfig.class)
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
 @SpringBootApplication
 public class Application implements InitializingBean {
 
@@ -28,5 +29,10 @@ public class Application implements InitializingBean {
   @Override
   public void afterPropertiesSet() throws Exception {
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
+
+  @Bean
+  public DateTimeProvider dateTimeProvider() {
+    return () -> Optional.of(OffsetDateTime.now());
   }
 }
