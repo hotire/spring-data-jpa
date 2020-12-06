5635922c06ce435fd885b7e1d944c87972873d51
hotire <gngh0101@gmail.com>
2020-12-07 00:17:53 +0900

    Implement PrePersist and PreUpdate

--- src/main/java/com/googlecode/hotire/springdatajpa/audit/CustomAuditingEntityListener.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/audit/CustomAuditingEntityListener.java
@@ -1,5 +1,8 @@
 package com.googlecode.hotire.springdatajpa.audit;
 
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -9,4 +12,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CustomAuditingEntityListener {
     private final AuditingEntityListener delegate;
+
+    @PrePersist
+    public void touchForCreate(Object target) {
+        delegate.touchForCreate(target);
+    }
+
+    @PreUpdate
+    public void touchForUpdate(Object target) {
+        delegate.touchForUpdate(target);
+    }
+
 }
