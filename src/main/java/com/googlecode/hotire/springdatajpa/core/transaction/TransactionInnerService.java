adf5038a489d75fd082b67a799e749590e835f36
hotire <gngh0101@gmail.com>
2021-03-22 00:35:27 +0900

    Add TransactionRepository

--- src/main/java/com/googlecode/hotire/springdatajpa/core/transaction/TransactionInnerService.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/transaction/TransactionInnerService.java
@@ -3,9 +3,11 @@ package com.googlecode.hotire.springdatajpa.core.transaction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class TransactionInnerService {
-
-
+    private final TransactionRepository transactionRepository;
 }
