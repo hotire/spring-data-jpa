d6b0e868d93d3daa78ea871fed12afb8b02a280e
hotire <gngh0101@gmail.com>
2021-04-08 00:12:47 +0900

    Use log instaed of sout

--- src/test/java/com/googlecode/hotire/springdatajpa/open/OpenEntityManagerServiceTest.java
+++ src/test/java/com/googlecode/hotire/springdatajpa/open/OpenEntityManagerServiceTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class OpenEntityManagerServiceTest {
 
@@ -13,8 +16,8 @@ class OpenEntityManagerServiceTest {
     @Test
     void select() {
         openEntityManagerService.select();
-        System.out.println("clear");
-        System.out.println("clear");
+        log.info("clear");
+        log.info("clear");
         openEntityManagerService.selectWithOpenEntityManager();
     }
 }
