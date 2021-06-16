01a851c7dbb064362f8bfbd53cc9b392712f78cb
hotire <gngh0101@gmail.com>
2021-06-17 00:05:35 +0900

    Update comment

--- src/main/java/com/googlecode/hotire/springdatajpa/exists/ExistsEntityRepository.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/exists/ExistsEntityRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ExistsEntityRepository extends JpaRepository<ExistsEntity, Long> {
+
+    // limit 1
     boolean existsByName(String name);
 
 //    @Query("SELECT EXIST (SELECT e.id FROM ExistsEntity e WHERE e.name =:name)") -> Validation failed for query for method public abstract boolean
 
+    // 성능상 이슈
     @Query("SELECT COUNT(e.id) > 0 FROM ExistsEntity e WHERE e.name =:name")
     boolean existsByQuery(String name);
 }
