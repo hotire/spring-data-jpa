cc597b3cf102b008a2f542c431ac8c7569eaafff
hotire <gngh0101@gmail.com>
2021-07-12 00:01:23 +0900

    Add prepared statement example

--- src/main/java/com/googlecode/hotire/springdatajpa/query/type/TypeService.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/query/type/TypeService.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -22,9 +23,58 @@ public class TypeService {
 
     @PostConstruct
     public void init() {
-//        typeRepository.saveAll(List.of(Type.builder().owner(Owner.builder().name("a").build()).build(), Type.builder().owner(Owner.builder().name("a").build()).build()));
-//        typeRepository.flush();
+        typeRepository.saveAll(List.of(Type.builder().owner(Owner.builder().name("a").build()).build(), Type.builder().owner(Owner.builder().name("a").build()).build()));
+        typeRepository.flush();
     }
+
+    public List<Type> findByOwnerName(String ownerName) {
+        TypedQuery<Type> query = entityManager.createQuery(
+                "SELECT t FROM Type t WHERE t.owner.name = '" + ownerName + "'", Type.class);
+        return query.getResultList();
+    }
+
+    public List<Type> findByOwnerNameWithPrepared(String ownerName) {
+        TypedQuery<Type> query = entityManager.createQuery(
+                "SELECT t FROM Type t WHERE t.owner.name = ?1", Type.class);
+        query.setParameter(1, ownerName);
+        return query.getResultList();
+    }
+
+    public List<Type> findByOwnerNameAndIdByCb(Long id, String ownerName) {
+        final CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        final CriteriaQuery<Type> query = builder.createQuery(Type.class);
+        final Root<Type> root = query.from(Type.class);
+        query.select(root).where(
+                builder.and(
+                        builder.equal(root.get(TypeSpecs.ID.getProperty()), id),
+                        builder.equal(root.get(TypeSpecs.OWNER.getProperty()).get(OwnerSpecs.NAME.getProperty()), ownerName
+                )
+        ));
+
+        TypedQuery<Type> typedQuery = entityManager.createQuery(query);
+        return typedQuery.getResultList();
+    }
+
+    public List<Type> findByOwnerNameAndIdByCbWithParam(Long id, String ownerName) {
+        final CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        final CriteriaQuery<Type> query = builder.createQuery(Type.class);
+        final Root<Type> root = query.from(Type.class);
+        ParameterExpression<String> paramOwner = builder.parameter(String.class);
+        ParameterExpression<Long> paramId = builder.parameter(Long.class);
+        query.select(root).where(
+                builder.and(
+                        builder.equal(root.get(TypeSpecs.ID.getProperty()), paramId),
+                        builder.equal(root.get(TypeSpecs.OWNER.getProperty()).get(OwnerSpecs.NAME.getProperty()), paramOwner
+                )
+                )
+        );
+
+        TypedQuery<Type> typedQuery = entityManager.createQuery(query);
+        typedQuery.setParameter(paramId, id);
+        typedQuery.setParameter(paramOwner, ownerName);
+        return typedQuery.getResultList();
+    }
+
 
     /**
      * criteriaBuilder
@@ -35,7 +85,11 @@ public class TypeService {
         final Root<Type> root = query.from(Type.class);
 
         final Predicate where = builder.or(types.stream()
-                                                .map(it -> builder.and(builder.equal(root.get(TypeSpecs.ID.getProperty()), it.getId()), builder.equal(root.get(TypeSpecs.OWNER.getProperty()).get(OwnerSpecs.NAME.getProperty()), it.getOwner().getName())))
+                                                .map(it -> builder.and(
+                                                        builder.equal(root.get(TypeSpecs.ID.getProperty()), it.getId()),
+                                                        builder.equal(root.get(TypeSpecs.OWNER.getProperty()).get(OwnerSpecs.NAME.getProperty()), it.getOwner().getName())
+                                                     )
+                                                )
                                                 .toArray(Predicate[]::new));
 
         final TypedQuery<Type> typedQuery = entityManager.createQuery(query.select(root).where(where));
--- src/test/java/com/googlecode/hotire/springdatajpa/query/type/TypeServiceTest.java
+++ src/test/java/com/googlecode/hotire/springdatajpa/query/type/TypeServiceTest.java
@@ -23,4 +23,24 @@ class TypeServiceTest {
         final List<Type> types = typeRepository.findAll();
         log.info("{}", typeService.findAll(types));
     }
+
+    @Test
+    void findByOwnerName() {
+        log.info("{}", typeService.findByOwnerName("a"));
+    }
+
+    @Test
+    void findByOwnerNameWithPrepared() {
+        log.info("{}", typeService.findByOwnerNameWithPrepared("a"));
+    }
+
+    @Test
+    void findByOwnerNameAndIdByCb() {
+        log.info("{}", typeService.findByOwnerNameAndIdByCb(1L, "a"));
+    }
+
+    @Test
+    void findByOwnerNameAndIdByCbWithParam() {
+        log.info("{}", typeService.findByOwnerNameAndIdByCbWithParam(1L,"a"));
+    }
 }
