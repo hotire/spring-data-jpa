ab86526538b4bc16d4fad068387caba11c882762
hotire <gngh0101@gmail.com>
2021-07-11 21:59:31 +0900

    Enabled Test

--- src/test/java/com/googlecode/hotire/springdatajpa/query/type/TypeServiceTest.java
+++ src/test/java/com/googlecode/hotire/springdatajpa/query/type/TypeServiceTest.java
@@ -2,14 +2,12 @@ package com.googlecode.hotire.springdatajpa.query.type;
 
 import java.util.List;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import lombok.extern.slf4j.Slf4j;
 
-@DisplayName("development")
 @Slf4j
 @SpringBootTest
 class TypeServiceTest {
