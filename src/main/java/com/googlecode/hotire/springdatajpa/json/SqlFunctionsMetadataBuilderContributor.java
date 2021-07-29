9676ace78ead767af87ccdd493beea9767a97a08
hotire <gngh0101@gmail.com>
2021-07-30 01:16:55 +0900

    Add comment to SqlFunctionsMetadataBuilderContributor

--- src/main/java/com/googlecode/hotire/springdatajpa/json/SqlFunctionsMetadataBuilderContributor.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/json/SqlFunctionsMetadataBuilderContributor.java
@@ -5,6 +5,9 @@ import org.hibernate.boot.spi.MetadataBuilderContributor;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.type.StandardBasicTypes;
 
+/**
+ * @see org.hibernate.jpa.boot.internal.SpringHibernateJpaPersistenceProvider#applyMetadataBuilderContributor
+ */
 public class SqlFunctionsMetadataBuilderContributor implements MetadataBuilderContributor {
     @Override
     public void contribute(final MetadataBuilder metadataBuilder) {
