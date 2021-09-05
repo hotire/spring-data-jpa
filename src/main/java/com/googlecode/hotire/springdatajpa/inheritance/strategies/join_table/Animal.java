0a52dce13f9cff6f4618b0d16f09a26d1a5efe01
hotire <gngh0101@gmail.com>
2021-09-05 12:10:49 +0900

    Add insertable, updatable to type in Animal

--- src/main/java/com/googlecode/hotire/springdatajpa/inheritance/strategies/join_table/Animal.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/inheritance/strategies/join_table/Animal.java
@@ -1,18 +1,25 @@
 package com.googlecode.hotire.springdatajpa.inheritance.strategies.join_table;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "type")
 public class Animal {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String species;
+    @Column(name = "type", insertable = false, updatable = false)
     private String type;
-    private String animalId;
+    private Long animalId;
 }
