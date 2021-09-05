d7012fea1d61fdb5ec51c6b9f8997f9b4129312c
hotire <gngh0101@gmail.com>
2021-09-06 00:29:53 +0900

    Verify save by DynamicTest

--- src/test/java/com/googlecode/hotire/springdatajpa/inheritance/strategies/join_table/AnimalRepositoryTest.java
+++ src/test/java/com/googlecode/hotire/springdatajpa/inheritance/strategies/join_table/AnimalRepositoryTest.java
@@ -1,19 +1,29 @@
 package com.googlecode.hotire.springdatajpa.inheritance.strategies.join_table;
 
-import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @DataJpaTest
 class AnimalRepositoryTest {
 
     @Autowired
     private AnimalRepository repository;
 
-    @Test
-    void save() {
-        final Animal animal = new Animal();
-        repository.saveAndFlush(animal);
+    @TestFactory
+    Stream<DynamicTest> save() {
+        return List.of(new Animal(), new Dog(), new Cat())
+                   .stream()
+                   .map(it -> DynamicTest.dynamicTest("save, param : " + it, () -> {
+                       final var result = repository.saveAndFlush(it);
+                       log.info("result : {}", result);
+                   }));
     }
-
 }
