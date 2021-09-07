854f2c77081a76f4a8eda300695b3b3f7064e0a1
hotire <gngh0101@gmail.com>
2021-09-08 00:14:23 +0900

    Add TablePerClassA

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/inheritance/strategies/table_per_class/TablePerClassA.java
@@ -0,0 +1,7 @@
+package com.googlecode.hotire.springdatajpa.inheritance.strategies.table_per_class;
+
+import javax.persistence.Entity;
+
+@Entity
+public class TablePerClassA extends TablePerClass{
+}
