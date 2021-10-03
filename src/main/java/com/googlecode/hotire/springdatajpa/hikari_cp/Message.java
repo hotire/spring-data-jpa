d05119611d29cdc8675089a42ed4c74aab2059fa
hotire <gngh0101@gmail.com>
2021-10-03 16:16:19 +0900

    Fix GeneratedValue

--- src/main/java/com/googlecode/hotire/springdatajpa/hikari_cp/Message.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/hikari_cp/Message.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @Setter
 class Message {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String contents;
