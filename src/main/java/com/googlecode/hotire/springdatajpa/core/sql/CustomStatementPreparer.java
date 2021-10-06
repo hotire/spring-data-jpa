ef27aaec25d2375726d3bc7e715fc375ddb08225
hotire <gngh0101@gmail.com>
2021-10-07 00:57:17 +0900

    Add CustomStatementPreparer

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/sql/CustomStatementPreparer.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.sql;
+
+/**
+ * prepareStatement
+ *
+ * @see org.hibernate.engine.jdbc.spi.StatementPreparer
+ * @see org.hibernate.engine.jdbc.internal.StatementPreparerImpl#prepareQueryStatement
+ */
+public class CustomStatementPreparer {
+}
---
+++ src/main/java/com/googlecode/hotire/springdatajpa/persist/DefaultPersistEventListenerDecorator.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.persist;
+
+import org.hibernate.event.spi.PersistEvent;
+
+/**
+ * @see org.hibernate.event.internal.DefaultPersistEventListener
+ * @see org.hibernate.event.internal.DefaultPersistEventListener#onPersist(PersistEvent)
+ */
+public class DefaultPersistEventListenerDecorator {
+}
