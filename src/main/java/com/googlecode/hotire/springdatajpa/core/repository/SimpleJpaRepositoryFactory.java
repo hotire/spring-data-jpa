79b545dd68c5c9bf0c00a276718f3b01a99931cc
hotire <gngh0101@gmail.com>
2021-10-12 00:15:26 +0900

    Add SimpleJpaRepositoryFactory

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/SimpleJpaRepositoryFactory.java
@@ -0,0 +1,29 @@
+package com.googlecode.hotire.springdatajpa.core.repository;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.repository.core.RepositoryInformation;
+import org.springframework.data.util.ReflectionUtils;
+
+/**
+ * @see org.springframework.data.jpa.repository.support.SimpleJpaRepository
+ */
+public class SimpleJpaRepositoryFactory {
+
+    /**
+     * @see org.springframework.data.repository.core.support.RepositoryFactorySupport#getTargetRepositoryViaReflection(RepositoryInformation, Object...)
+     */
+    @SuppressWarnings("unchecked")
+    public static <R> R getTargetRepositoryViaReflection(final Class<?> baseClass, final Object... constructorArguments) {
+        final Optional<Constructor<?>> constructor = ReflectionUtils.findConstructor(baseClass, constructorArguments);
+
+        return constructor.map(it -> (R) BeanUtils.instantiateClass(it, constructorArguments))
+                          .orElseThrow(() -> new IllegalStateException(String.format(
+                                  "No suitable constructor found on %s to match the given arguments: %s. Make sure you implement a constructor taking these",
+                                  baseClass, Arrays.stream(constructorArguments).map(Object::getClass).collect(Collectors.toList()))));
+    }
+}
