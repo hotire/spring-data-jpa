758222c886315ef2f4d44db4173503f6ae8263d8
hotire <gngh0101@gmail.com>
2022-02-07 00:24:35 +0900

    Add PESSIMISTIC_WRITE of entityManager

--- src/test/java/com/googlecode/hotire/springdatajpa/lock/LockEntityRepositoryTest.java
+++ src/test/java/com/googlecode/hotire/springdatajpa/lock/LockEntityRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.googlecode.hotire.springdatajpa.lock;
 
+import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,9 +13,15 @@ class LockEntityRepositoryTest {
     @Autowired
     private LockEntityRepository repository;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Test
     void findByIdForUpdate() {
         final LockEntity result = repository.saveAndFlush(new LockEntity());
+
+        entityManager.find(LockEntity.class, result.getId(), LockModeType.PESSIMISTIC_WRITE);
         repository.findByIdForUpdate(result.getId());
     }
+
 }
