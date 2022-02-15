afb576bccf4403168864d130ada1e99525f97366
hotire <gngh0101@gmail.com>
2022-02-16 00:54:59 +0900

    Add ApplicationListenerMethodTransactionalAdapter

--- src/main/java/com/googlecode/hotire/springdatajpa/event/simple/SimpleAggregateRootEventListener.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/event/simple/SimpleAggregateRootEventListener.java
@@ -2,9 +2,14 @@ package com.googlecode.hotire.springdatajpa.event.simple;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * @see org.springframework.beans.factory.config.BeanFactoryPostProcessor
+ * @see org.springframework.context.event.EventListenerMethodProcessor
+ */
 @Slf4j
 @Component
 public class SimpleAggregateRootEventListener {
@@ -12,5 +17,13 @@ public class SimpleAggregateRootEventListener {
     @EventListener
     public void listen(final SimpleAggregateRoot root) {
         log.info("SimpleAggregateRoot");
+    }
+
+    /**
+     * @see org.springframework.transaction.event.ApplicationListenerMethodTransactionalAdapter
+     */
+    @TransactionalEventListener
+    public void transactionalEventListen(final SimpleAggregateRoot root) {
+
     }
 }
