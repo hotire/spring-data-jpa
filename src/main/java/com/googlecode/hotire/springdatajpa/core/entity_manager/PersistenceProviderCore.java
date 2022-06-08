6ce94000bac9048b8f4723cb344f85f60cee9f97
hotire <gngh0101@gmail.com>
2022-06-09 00:15:01 +0900

    Add PersistenceProviderCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/entity_manager/PersistenceProviderCore.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.springdatajpa.core.entity_manager;
+
+/**
+ * @see javax.persistence.spi.PersistenceProvider
+ * @see org.hibernate.jpa.HibernatePersistenceProvider
+ * @see org.springframework.orm.jpa.vendor.SpringHibernateJpaPersistenceProvider
+ */
+public class PersistenceProviderCore {
+}
