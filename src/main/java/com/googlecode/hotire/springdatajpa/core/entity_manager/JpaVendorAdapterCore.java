ab3c97e44196afeaf867033ed66965a65e78fd51
hotire <gngh0101@gmail.com>
2022-06-09 00:14:40 +0900

    Add JpaVendorAdapterCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/entity_manager/JpaVendorAdapterCore.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.springdatajpa.core.entity_manager;
+
+/**
+ * @see org.springframework.orm.jpa.JpaVendorAdapter
+ * @see org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter
+ * @see org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+ */
+public class JpaVendorAdapterCore {
+}
