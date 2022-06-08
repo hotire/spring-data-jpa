0848b543bfab55bee63d0f6adc196f6911055d16
hotire <gngh0101@gmail.com>
2022-06-09 00:14:22 +0900

    Add EntityManagerCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/entity_manager/EntityManagerCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.entity_manager;
+
+/**
+ * @see javax.persistence.EntityManager
+ * @see org.hibernate.Session
+ * @see org.hibernate.engine.spi.SessionImplementor
+ * @see org.hibernate.internal.SessionImpl
+ */
+public class EntityManagerCore {
+}
