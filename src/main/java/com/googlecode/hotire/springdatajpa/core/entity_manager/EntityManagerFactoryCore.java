92fced26a3aac0b4c5b99bb60bc40f9cd101c166
hotire <gngh0101@gmail.com>
2022-06-13 00:37:53 +0900

    Add EntityManagerFactoryCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/entity_manager/EntityManagerFactoryCore.java
@@ -0,0 +1,12 @@
+package com.googlecode.hotire.springdatajpa.core.entity_manager;
+
+/**
+ * @see javax.persistence.EntityManagerFactory
+ * @see org.hibernate.SessionFactory
+ * @see org.hibernate.internal.SessionFactoryImpl
+ *
+ * @see  org.springframework.orm.jpa.AbstractEntityManagerFactoryBean -> FactoryBean<EntityManagerFactory>
+ * @see  org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+ */
+public class EntityManagerFactoryCore {
+}
