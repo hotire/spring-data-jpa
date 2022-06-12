64111dc00fa4bc9f344ba17cfde2c7d9acc76c38
hotire <gngh0101@gmail.com>
2022-06-13 00:37:19 +0900

    Add TransactionalCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/transaction/TransactionalCore.java
@@ -0,0 +1,11 @@
+package com.googlecode.hotire.springdatajpa.core.transaction;
+
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @see Transactional
+ * @see org.springframework.transaction.annotation.Propagation
+ * @see org.springframework.transaction.annotation.Isolation
+ */
+public class TransactionalCore {
+}
