2040742c90ed9cb64dff4167cf25205d9dbb8461
hotire <gngh0101@gmail.com>
2022-06-14 00:41:52 +0900

    Add QueryExecutorMethodInterceptorCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/QueryExecutorMethodInterceptorCore.java
@@ -0,0 +1,7 @@
+package com.googlecode.hotire.springdatajpa.core.repository;
+
+/**
+ * @see org.springframework.data.repository.core.support.RepositoryFactorySupport.QueryExecutorMethodInterceptor
+ */
+public class QueryExecutorMethodInterceptorCore {
+}
