43f0070565e529dd365e9fd19a227f6296059b9a
hotire <gngh0101@gmail.com>
2022-06-15 23:38:41 +0900

    Add TransactionalEventListenerCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/event/core/TransactionalEventListenerCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.event.core;
+
+/**
+ * @see org.springframework.transaction.event.TransactionalEventListener
+ * @see org.springframework.context.event.EventListenerMethodProcessor
+ * @see org.springframework.transaction.event.TransactionalEventListenerFactory
+ * @see org.springframework.transaction.event.ApplicationListenerMethodTransactionalAdapter
+ */
+public class TransactionalEventListenerCore {
+}
