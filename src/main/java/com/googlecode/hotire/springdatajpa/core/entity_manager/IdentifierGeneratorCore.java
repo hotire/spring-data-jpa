e32d1e08bb35c5c495bd97e1ebbfad5127e2539b
hotire <gngh0101@gmail.com>
2022-06-15 23:36:47 +0900

    Add IdentifierGeneratorCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/entity_manager/IdentifierGeneratorCore.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.springdatajpa.core.entity_manager;
+
+/**
+ * @see org.hibernate.id.IdentifierGenerator
+ * @see org.hibernate.id.AbstractPostInsertGenerator
+ * @see org.hibernate.id.IdentifierGeneratorHelper
+ */
+public class IdentifierGeneratorCore {
+}
