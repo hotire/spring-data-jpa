cbe1999fd6a76d9464c7e428a752ddbca1bde409
hotire <gngh0101@gmail.com>
2022-06-17 00:54:24 +0900

    Add TransactionSynchronizationCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/event/core/TransactionSynchronizationCore.java
@@ -0,0 +1,7 @@
+package com.googlecode.hotire.springdatajpa.event.core;
+
+/**
+ * @see org.springframework.transaction.support.TransactionSynchronization
+ */
+public class TransactionSynchronizationCore {
+}
