f3cd15994da48d15d0c514d3822681a6841f21be
hotire <gngh0101@gmail.com>
2022-06-19 21:35:00 +0900

    Add CallbackRegistryCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/event_listener/CallbackRegistryCore.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.springdatajpa.event_listener;
+
+/**
+ * @see org.hibernate.jpa.event.spi.CallbackRegistry
+ * @see org.hibernate.jpa.event.internal.CallbackRegistryImpl
+ * @see org.hibernate.jpa.event.internal.CallbackBuilderLegacyImpl
+ */
+public class CallbackRegistryCore {
+}
