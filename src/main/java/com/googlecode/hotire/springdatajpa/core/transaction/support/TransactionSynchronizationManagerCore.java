83439c48c412558643f77f92cd80670a2dbd5bfa
hotire <gngh0101@gmail.com>
2022-07-15 16:03:08 +0900

    Add TransactionSynchronizationManager

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/transaction/support/TransactionSynchronizationManagerCore.java
@@ -0,0 +1,7 @@
+package com.googlecode.hotire.springdatajpa.core.transaction.support;
+
+/**
+ * @see org.springframework.transaction.support.TransactionSynchronizationManager
+ */
+public class TransactionSynchronizationManagerCore {
+}
