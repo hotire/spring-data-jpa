cfcfa8c5330471439a60d368e8b0744534e06a50
hotire <gngh0101@gmail.com>
2022-07-17 23:48:41 +0900

    Add QueryMethodCore.java

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/QueryMethodCore.java
@@ -0,0 +1,8 @@
+package com.googlecode.hotire.springdatajpa.core.repository.method;
+
+/**
+ * @see org.springframework.data.repository.query.QueryMethod
+ * @see org.springframework.data.jpa.repository.query.JpaQueryMethod
+ */
+public class QueryMethodCore {
+}
