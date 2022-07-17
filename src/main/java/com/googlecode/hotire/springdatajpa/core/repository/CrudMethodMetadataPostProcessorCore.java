c4853334ac29ebf52f656ce03b78549e4c0c32a7
hotire <gngh0101@gmail.com>
2022-07-17 15:19:36 +0900

    Add CrudMethodMetadataPostProcessorCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/CrudMethodMetadataPostProcessorCore.java
@@ -0,0 +1,7 @@
+package com.googlecode.hotire.springdatajpa.core.repository;
+
+/**
+ * @see org.springframework.data.jpa.repository.support.CrudMethodMetadataPostProcessor
+ */
+public class CrudMethodMetadataPostProcessorCore {
+}
