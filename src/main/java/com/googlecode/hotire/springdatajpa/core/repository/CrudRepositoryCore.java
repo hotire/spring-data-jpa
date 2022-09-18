62409d9ab4e8c6e787572b3b9c3b2f0b3bec1102
hotire <gngh0101@gmail.com>
2022-09-19 00:07:17 +0900

    Update CrudRepositoryCore

--- src/main/java/com/googlecode/hotire/springdatajpa/core/repository/CrudRepositoryCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/CrudRepositoryCore.java
@@ -1,10 +1,18 @@
 package com.googlecode.hotire.springdatajpa.core.repository;
 
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.data.repository.CrudRepository;
+
 
 /**
  * @see CrudRepository
  */
-public class CrudRepositoryCore {
+public interface CrudRepositoryCore<T> {
+
+  /**
+   * @see CrudRepository#deleteAll(Iterable)
+   * @see SimpleJpaRepository#deleteAll(Iterable)
+   */
+  void deleteAll(Iterable<? extends T> entities);
 
 }
