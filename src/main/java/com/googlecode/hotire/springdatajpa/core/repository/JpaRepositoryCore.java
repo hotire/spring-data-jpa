d160e7230abc7c7a765cebc0e2ee7bb51eba82ae
hotire <gngh0101@gmail.com>
2022-09-19 00:08:43 +0900

    Add JpaRepositoryCore

--- src/main/java/com/googlecode/hotire/springdatajpa/core/repository/JpaRepositoryCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/JpaRepositoryCore.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * @see JpaRepository
  */
-public class JpaRepositoryCore {
+public interface JpaRepositoryCore {
+
+  /**
+   * @see JpaRepository#deleteInBatch(Iterable) 
+   */
+  void deleteInBatch(Iterable<T> entities);
 
 }
