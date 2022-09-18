eee88985872c3ca116d96856d918d132b5e268ce
hotire <gngh0101@gmail.com>
2022-09-19 00:08:13 +0900

    Add JpaRepositoryCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/JpaRepositoryCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @see JpaRepository
+ */
+public class JpaRepositoryCore {
+
+}
