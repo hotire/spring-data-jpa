cf039980b0c70fcd76c74a86df5cfa14a690b947
hotire <gngh0101@gmail.com>
2022-09-18 23:05:30 +0900

    Add CrudRepositoryCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/CrudRepositoryCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @see CrudRepository
+ */
+public class CrudRepositoryCore {
+
+}
