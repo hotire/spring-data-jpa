add306422d5435c3e1f22c74b34feb049fdef856
hotire <gngh0101@gmail.com>
2022-09-19 00:15:56 +0900

    Implement deleteInBatch

--- src/main/java/com/googlecode/hotire/springdatajpa/core/repository/JpaRepositoryCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/JpaRepositoryCore.java
@@ -1,6 +1,13 @@
 package com.googlecode.hotire.springdatajpa.core.repository;
 
+import static org.springframework.data.jpa.repository.query.QueryUtils.DELETE_ALL_QUERY_STRING;
+import static org.springframework.data.jpa.repository.query.QueryUtils.applyAndBind;
+import static org.springframework.data.jpa.repository.query.QueryUtils.getQueryString;
+
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 
@@ -8,12 +15,19 @@ import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
  * @see JpaRepository
  * @see SimpleJpaRepository
  */
-public interface JpaRepositoryCore<T> {
+@RequiredArgsConstructor
+public class JpaRepositoryCore<T> {
+
+  private final JpaEntityInformation<T, ?> entityInformation;
+  private final EntityManager em;
 
   /**
    * @see JpaRepository#deleteInBatch(Iterable) 
    * @see SimpleJpaRepository#deleteInBatch(Iterable) 
    */
-  void deleteInBatch(Iterable<T> entities);
+  void deleteInBatch(Iterable<T> entities) {
+    applyAndBind(getQueryString(DELETE_ALL_QUERY_STRING, entityInformation.getEntityName()), entities, em)
+        .executeUpdate();
+  }
 
 }
