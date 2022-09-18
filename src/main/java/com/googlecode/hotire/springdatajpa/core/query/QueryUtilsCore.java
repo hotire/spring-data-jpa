469d9160d64645564204b2cdb9eb84753f734128
hotire <gngh0101@gmail.com>
2022-09-19 00:11:23 +0900

    Add QueryUtilsCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/query/QueryUtilsCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.query;
+
+import org.springframework.data.jpa.repository.query.QueryUtils;
+
+/**
+ * @see QueryUtils
+ */
+public class QueryUtilsCore {
+
+}
