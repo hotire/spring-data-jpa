7cc0aec7063ce2b10f36d3508535f7ac4b0bfe18
hotire <gngh0101@gmail.com>
2022-09-19 00:09:39 +0900

    Learn deleteInBatch

--- src/main/java/com/googlecode/hotire/springdatajpa/core/repository/JpaRepositoryCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/JpaRepositoryCore.java
@@ -1,14 +1,18 @@
 package com.googlecode.hotire.springdatajpa.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
 
 /**
  * @see JpaRepository
+ * @see SimpleJpaRepository
  */
 public interface JpaRepositoryCore<T> {
 
   /**
    * @see JpaRepository#deleteInBatch(Iterable) 
+   * @see SimpleJpaRepository#deleteInBatch(Iterable) 
    */
   void deleteInBatch(Iterable<T> entities);
 
