0e9a2313613b6df9daefdd20d16044bbf9321a25
hotire <gngh0101@gmail.com>
2022-09-22 03:43:58 +0900

    Add JpaMetamodelEntityInformation, JpaPersistableEntityInformation

--- src/main/java/com/googlecode/hotire/springdatajpa/save/JpaEntityInformationSupportCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/save/JpaEntityInformationSupportCore.java
@@ -3,10 +3,14 @@ package com.googlecode.hotire.springdatajpa.save;
 import javax.persistence.EntityManager;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.JpaEntityInformationSupport;
+import org.springframework.data.jpa.repository.support.JpaMetamodelEntityInformation;
+import org.springframework.data.jpa.repository.support.JpaPersistableEntityInformation;
 
 /**
  * @see JpaEntityInformation
  * @see JpaEntityInformationSupport
+ * @see JpaMetamodelEntityInformation
+ * @see JpaPersistableEntityInformation
  */
 public class JpaEntityInformationSupportCore {
 
