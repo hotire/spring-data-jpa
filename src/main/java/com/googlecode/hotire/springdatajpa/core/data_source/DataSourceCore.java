fed534fc1312b3c633b81b6ec70606dc50e3c4e2
hotire <gngh0101@gmail.com>
2022-10-01 14:23:57 +0900

    Add getConnection

--- src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/DataSourceCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/DataSourceCore.java
@@ -1,9 +1,22 @@
 package com.googlecode.hotire.springdatajpa.core.data_source;
+import java.sql.Connection;
+import java.sql.SQLException;
 import javax.sql.DataSource;
 
 /**
  * @see DataSource
  */
 public interface DataSourceCore {
+
+    /**
+     * @see DataSource#getConnection()
+     */
+    Connection getConnection() throws SQLException;
+
+    /**
+     * @see DataSource#getConnection(String, String) 
+     */
+    Connection getConnection(String username, String password)
+        throws SQLException;
 
 }
