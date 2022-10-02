3246c34877d47c9effc63504d43404c7a5b3fa42
hotire <gngh0101@gmail.com>
2022-10-02 17:01:20 +0900

    Add AbstractDataSource

--- src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/DataSourceCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/DataSourceCore.java
@@ -2,19 +2,21 @@ package com.googlecode.hotire.springdatajpa.core.data_source;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.AbstractDataSource;
 
 /**
  * @see DataSource
+ * @see AbstractDataSource
  */
 public interface DataSourceCore {
 
     /**
-     * @see DataSource#getConnection()
+     * @see DataSource#get   Connection()
      */
     Connection getConnection() throws SQLException;
 
     /**
-     * @see DataSource#getConnection(String, String) 
+     * @see DataSource#getConnection(String, String)
      */
     Connection getConnection(String username, String password)
         throws SQLException;
