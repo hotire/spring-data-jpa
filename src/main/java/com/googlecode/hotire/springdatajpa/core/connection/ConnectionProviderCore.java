0326ef6a7fea2cd151b8f8521bca2dc7f2376f94
hotire <gngh0101@gmail.com>
2022-10-03 18:13:46 +0900

    Add ConnectionProvider

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/connection/ConnectionProviderCore.java
@@ -0,0 +1,13 @@
+package com.googlecode.hotire.springdatajpa.core.connection;
+
+
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
+
+/**
+ * @see ConnectionProvider
+ * @see DatasourceConnectionProviderImpl
+ */
+public class ConnectionProviderCore {
+
+}
