85cdcf92dd40df0d3358c7e5ea379dcefc16ba14
hotire <gngh0101@gmail.com>
2022-10-04 00:28:35 +0900

    Simplify

--- src/main/java/com/googlecode/hotire/springdatajpa/core/connection/ConnectionProviderCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/connection/ConnectionProviderCore.java
@@ -15,7 +15,6 @@ public interface ConnectionProviderCore {
     /**
      * @see ConnectionProvider#getConnection()
      * @see DatasourceConnectionProviderImpl#getConnection()
-     * @see DatasourceConnectionProviderImpl#getConnection()
      */
     Connection getConnection() throws SQLException;
 }
