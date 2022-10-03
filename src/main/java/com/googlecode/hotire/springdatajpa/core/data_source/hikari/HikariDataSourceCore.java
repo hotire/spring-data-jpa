8ddce2a0ff58c5a672588c3bdd74247e62222dbd
hotire <gngh0101@gmail.com>
2022-10-03 11:20:42 +0900

    Add HikariDataSource

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/hikari/HikariDataSourceCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.data_source.hikari;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+/**
+ * @see HikariDataSource
+ */
+public class HikariDataSourceCore {
+
+}
