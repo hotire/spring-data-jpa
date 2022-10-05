8d0ab593312b2839449f224dafac594d2f25dab1
hotire <gngh0101@gmail.com>
2022-10-05 10:24:58 +0900

    Add HikariPoolCore

--- src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/hikari/HikariDataSourceCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/hikari/HikariDataSourceCore.java
@@ -1,10 +1,12 @@
 package com.googlecode.hotire.springdatajpa.core.data_source.hikari;
 
 import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.pool.HikariPool;
 
 /**
  * @see HikariDataSource
  */
 public class HikariDataSourceCore {
 
+    private volatile HikariPool pool;
 }
---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/hikari/HikariPoolCore.java
@@ -0,0 +1,8 @@
+package com.googlecode.hotire.springdatajpa.core.data_source.hikari;
+
+/**
+ * @see  com.zaxxer.hikari.pool.HikariPool
+ */
+public class HikariPoolCore {
+
+}
