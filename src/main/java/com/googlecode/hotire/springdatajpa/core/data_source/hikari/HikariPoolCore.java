ea9c3164e9b1ec9c44248f25619a3a5432c710ea
hotire <gngh0101@gmail.com>
2022-10-06 10:15:08 +0900

    Learn getConnection in HikariPoolCore

--- src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/hikari/HikariPoolCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/hikari/HikariPoolCore.java
@@ -1,8 +1,17 @@
 package com.googlecode.hotire.springdatajpa.core.data_source.hikari;
 
-/**
- * @see  com.zaxxer.hikari.pool.HikariPool
- */
-public class HikariPoolCore {
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.zaxxer.hikari.pool.HikariPool;
 
+/**
+ * @see HikariPool
+ */
+public interface HikariPoolCore {
+
+    /**
+     * @see HikariPool#getConnection() 
+     * @see HikariPool#getConnection(long) () 
+     */
+    Connection getConnection() throws SQLException;
 }
