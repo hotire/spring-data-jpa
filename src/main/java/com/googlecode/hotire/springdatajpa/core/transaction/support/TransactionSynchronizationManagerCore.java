c1d26b4f398f9d88dba5b6a772aced88311eda73
hotire <gngh0101@gmail.com>
2022-10-12 01:47:39 +0900

    Leearn registerSynchronization

--- src/main/java/com/googlecode/hotire/springdatajpa/core/transaction/support/TransactionSynchronizationManagerCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/transaction/support/TransactionSynchronizationManagerCore.java
@@ -1,7 +1,18 @@
 package com.googlecode.hotire.springdatajpa.core.transaction.support;
 
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
 /**
- * @see org.springframework.transaction.support.TransactionSynchronizationManager
+ * @see TransactionSynchronizationManager
  */
 public class TransactionSynchronizationManagerCore {
+
+    /**
+     * @see TransactionSynchronizationManager#registerSynchronization(TransactionSynchronization) 
+     */
+    public static void registerSynchronization(TransactionSynchronization synchronization)
+        throws IllegalStateException {
+        TransactionSynchronizationManager.registerSynchronization(synchronization);
+    }
 }
