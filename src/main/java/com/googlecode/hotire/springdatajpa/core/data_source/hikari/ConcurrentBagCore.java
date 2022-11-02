97c680061fe063d465159e9b530bfc1026c6ee96
hotire <gngh0101@gmail.com>
2022-11-03 02:07:02 +0900

    Add ConcurrentBagCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/hikari/ConcurrentBagCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.data_source.hikari;
+
+import com.zaxxer.hikari.util.ConcurrentBag;
+
+/**
+ * @see ConcurrentBag
+ */
+public class ConcurrentBagCore {
+
+}
