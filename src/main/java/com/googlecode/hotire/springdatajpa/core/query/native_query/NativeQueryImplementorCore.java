e06420c7305458b0de5de7e0b3e621091f013293
hotire <gngh0101@gmail.com>
2022-11-04 01:59:00 +0900

    Add NativeQueryImplementorCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/query/native_query/NativeQueryImplementorCore.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.springdatajpa.core.query.native_query;
+import org.hibernate.query.spi.NativeQueryImplementor;
+
+/**
+ * @see NativeQueryImplementor
+ */
+public class NativeQueryImplementorCore {
+
+}
