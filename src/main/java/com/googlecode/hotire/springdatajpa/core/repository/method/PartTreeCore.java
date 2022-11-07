8018d9b599c682952d01a2f8f30c0d4326de7e4d
hotire <gngh0101@gmail.com>
2022-11-08 01:42:15 +0900

    Add PartTreeCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/PartTreeCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.repository.method;
+
+import org.springframework.data.repository.query.parser.PartTree;
+
+/**
+ * @see PartTree
+ */
+public class PartTreeCore {
+
+}
