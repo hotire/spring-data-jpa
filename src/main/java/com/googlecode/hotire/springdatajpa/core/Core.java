c65cdc391e41acc43bf762fc7282462262a081c4
hotire <gngh0101@gmail.com>
2022-11-12 14:01:35 +0900

    Add Access to core

--- src/main/java/com/googlecode/hotire/springdatajpa/core/Core.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/Core.java
@@ -1,5 +1,7 @@
 package com.googlecode.hotire.springdatajpa.core;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,5 +19,6 @@ public class Core {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Access(AccessType.FIELD)
     private String name;
 }
