51ac2cb1acb6a18254331c60d9bb08ce7024cddf
hotire <gngh0101@gmail.com>
2022-11-13 20:30:24 +0900

    Add XAnnotatedElement

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/access/PropertyInferredDataCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.access;
+
+import org.hibernate.cfg.PropertyInferredData;
+
+/**
+ * @see PropertyInferredData
+ */
+public class PropertyInferredDataCore {
+
+}
