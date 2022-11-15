19213f968daf41217db3d9abb3680d358af152bc
hotire <gngh0101@gmail.com>
2022-11-16 01:25:18 +0900

    Add ParameterBinderCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/ParameterBinderCore.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.springdatajpa.core.repository.method;
+import org.springframework.data.jpa.repository.query.ParameterBinder;
+
+/**
+ * @see ParameterBinder
+ */
+public class ParameterBinderCore {
+
+}
