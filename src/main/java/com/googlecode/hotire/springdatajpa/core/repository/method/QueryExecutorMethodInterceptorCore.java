3c7d938feae9b36a2c5145a2ea023ee05942ba87
hotire <gngh0101@gmail.com>
2022-11-17 00:21:48 +0900

    Learn lookupQuery in QueryExecutorMethodInterceptor

--- src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/QueryExecutorMethodInterceptorCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/QueryExecutorMethodInterceptorCore.java
@@ -1,7 +1,24 @@
 package com.googlecode.hotire.springdatajpa.core.repository.method;
 
+import java.lang.reflect.Method;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.repository.core.RepositoryInformation;
+import org.springframework.data.repository.query.QueryLookupStrategy;
+import org.springframework.data.repository.query.RepositoryQuery;
+import org.springframework.data.util.Pair;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport.QueryExecutorMethodInterceptor;
+
 /**
- * @see org.springframework.data.repository.core.support.RepositoryFactorySupport.QueryExecutorMethodInterceptor
+ * @see QueryExecutorMethodInterceptor
  */
 public class QueryExecutorMethodInterceptorCore {
+
+
+    /**
+     * @see QueryExecutorMethodInterceptor#lookupQuery(Method, RepositoryInformation, QueryLookupStrategy, ProjectionFactory)
+     */
+    private Pair<Method, RepositoryQuery> lookupQuery(Method method, RepositoryInformation information,
+        QueryLookupStrategy strategy, ProjectionFactory projectionFactory) {
+        return null;
+    }
 }
