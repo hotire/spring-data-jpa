ece938fc468a8734a2b09104eda6850515bc319b
hotire <gngh0101@gmail.com>
2022-11-17 00:21:22 +0900

    Add RepositoryFactorySupportCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/RepositoryFactorySupportCore.java
@@ -0,0 +1,27 @@
+package com.googlecode.hotire.springdatajpa.core.repository;
+
+
+import java.util.Optional;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+import org.springframework.data.repository.query.QueryLookupStrategy;
+import org.springframework.data.repository.query.QueryLookupStrategy.Key;
+import org.springframework.data.repository.query.QueryMethodEvaluationContextProvider;
+import org.springframework.lang.Nullable;
+
+
+/**
+ * @see RepositoryFactorySupport
+ * @see JpaRepositoryFactory
+ */
+public class RepositoryFactorySupportCore {
+
+    /**
+     * @see RepositoryFactorySupport#getQueryLookupStrategy(Key, QueryMethodEvaluationContextProvider)
+     * @see JpaRepositoryFactory#getQueryLookupStrategy(Key, QueryMethodEvaluationContextProvider) 
+     */
+    protected Optional<QueryLookupStrategy> getQueryLookupStrategy(@Nullable Key key,
+        QueryMethodEvaluationContextProvider evaluationContextProvider) {
+        return Optional.empty();
+    }
+}
