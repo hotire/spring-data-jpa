548ef6ecf4908a23611b4e4939d7b73c96d1df3a
hotire <gngh0101@gmail.com>
2022-11-18 01:21:30 +0900

    Add ConnectionHolderCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/ConnectionHolderCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.data_source;
+
+import org.springframework.jdbc.datasource.ConnectionHolder;
+
+/**
+ * @see ConnectionHolder
+ */
+public class ConnectionHolderCore {
+
+}
