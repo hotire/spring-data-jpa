509196ca4a41e7faae528e727dc68b219998a2e4
hotire <gngh0101@gmail.com>
2022-11-22 01:33:52 +0900

    Add LazyConnectionDataSourceProxy

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/LazyConnectionDataSourceProxyCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.data_source;
+
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+/**
+ * @see LazyConnectionDataSourceProxy
+ */
+public class LazyConnectionDataSourceProxyCore {
+
+}
