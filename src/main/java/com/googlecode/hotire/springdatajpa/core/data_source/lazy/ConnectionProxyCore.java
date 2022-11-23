4f12dcfce7ba1b258504dfc6a8417d3821da3b34
hotire <gngh0101@gmail.com>
2022-11-24 01:46:26 +0900

    Add ConnectionProxyCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/lazy/ConnectionProxyCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.data_source.lazy;
+
+import org.springframework.jdbc.datasource.ConnectionProxy;
+
+/**
+ * @see ConnectionProxy
+ */
+public class ConnectionProxyCore {
+
+}
