33f16081ee28de18fd89ec0a2053c50a4e5e0435
hotire <gngh0101@gmail.com>
2022-11-25 00:31:29 +0900

    Add getTargetConnection

--- src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/lazy/ConnectionProxyCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/lazy/ConnectionProxyCore.java
@@ -1,10 +1,18 @@
 package com.googlecode.hotire.springdatajpa.core.data_source.lazy;
 
+import java.sql.Connection;
 import org.springframework.jdbc.datasource.ConnectionProxy;
 
 /**
  * @see ConnectionProxy
  */
 public class ConnectionProxyCore {
+
+    /**
+     * @see ConnectionProxy#getTargetConnection()
+     */
+    public Connection getTargetConnection() {
+        return null;
+    }
 
 }
