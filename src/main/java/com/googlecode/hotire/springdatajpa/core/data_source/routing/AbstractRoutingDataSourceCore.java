6b8a081e9e5a650cdd30c3fd0fe461a41125e00a
hotire <gngh0101@gmail.com>
2022-11-28 01:16:57 +0900

    Add AbstractRoutingDataSource#targetDataSources

--- src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/routing/AbstractRoutingDataSourceCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/routing/AbstractRoutingDataSourceCore.java
@@ -1,5 +1,6 @@
 package com.googlecode.hotire.springdatajpa.core.data_source.routing;
 
+import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
@@ -7,6 +8,11 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  * @see AbstractRoutingDataSource
  */
 public class AbstractRoutingDataSourceCore {
+
+    /**
+     * @see AbstractRoutingDataSource#targetDataSources
+     */
+    private Map<Object, Object> targetDataSources;
 
     /**
      * @see AbstractRoutingDataSource#determineTargetDataSource()
