72613a7b0bd47dd523a2953bc8b0cf573b3b066c
hotire <gngh0101@gmail.com>
2022-11-27 11:19:02 +0900

    Add AbstractRoutingDataSource

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/routing/AbstractRoutingDataSourceCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.data_source.routing;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+/**
+ * @see AbstractRoutingDataSource
+ */
+public class AbstractRoutingDataSourceCore {
+
+}
