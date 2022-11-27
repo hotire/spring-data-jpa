5690073b3bccc506255bf60ec2235353df67b33e
hotire <gngh0101@gmail.com>
2022-11-27 11:21:13 +0900

    Add determineTargetDataSource

--- src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/routing/AbstractRoutingDataSourceCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/routing/AbstractRoutingDataSourceCore.java
@@ -1,10 +1,18 @@
 package com.googlecode.hotire.springdatajpa.core.data_source.routing;
 
+import javax.sql.DataSource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
  * @see AbstractRoutingDataSource
  */
 public class AbstractRoutingDataSourceCore {
+
+    /**
+     * @see AbstractRoutingDataSource#determineTargetDataSource()
+     */
+    protected DataSource determineTargetDataSource() {
+        return null;
+    }
 
 }
