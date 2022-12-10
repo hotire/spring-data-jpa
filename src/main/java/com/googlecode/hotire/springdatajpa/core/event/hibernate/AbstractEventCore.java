a2b5328a9b398bf75bcf7e4a7c0134950c7d8cad
hotire <gngh0101@gmail.com>
2022-12-10 16:03:25 +0900

    Add AbstractEventCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/AbstractEventCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.event.hibernate;
+
+import org.hibernate.event.spi.AbstractEvent;
+
+/**
+ * @see AbstractEvent
+ */
+public class AbstractEventCore {
+
+}
