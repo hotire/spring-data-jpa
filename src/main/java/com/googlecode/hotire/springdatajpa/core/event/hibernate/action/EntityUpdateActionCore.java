4683ce742169fa597560892b15ab19bda990d1e7
hotire <gngh0101@gmail.com>
2022-12-12 00:08:07 +0900

    Add EntityUpdateActionCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/action/EntityUpdateActionCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.event.hibernate.action;
+
+import org.hibernate.action.internal.EntityUpdateAction;
+
+/**
+ * @see EntityUpdateAction
+ */
+public class EntityUpdateActionCore {
+
+}
