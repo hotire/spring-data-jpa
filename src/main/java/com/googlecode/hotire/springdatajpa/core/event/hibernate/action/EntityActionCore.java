b1c01ed3f54d502e87ac786b2db0659300852ec8
hotire <gngh0101@gmail.com>
2022-12-12 00:07:24 +0900

    Add EntityActionCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/action/EntityActionCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.event.hibernate.action;
+
+import org.hibernate.action.internal.EntityAction;
+
+/**
+ * @see EntityAction
+ */
+public class EntityActionCore {
+
+}
