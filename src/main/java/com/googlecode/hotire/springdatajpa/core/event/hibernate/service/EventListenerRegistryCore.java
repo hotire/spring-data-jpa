f096e089acca98e213df21955a29433daf08a9c9
hotire <gngh0101@gmail.com>
2022-12-13 02:09:24 +0900

    Add EventListenerRegistryImpl

--- src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/service/EventListenerRegistryCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/service/EventListenerRegistryCore.java
@@ -1,9 +1,11 @@
 package com.googlecode.hotire.springdatajpa.core.event.hibernate.service;
 
 import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.service.internal.EventListenerRegistryImpl;
 
 /**
  * @see EventListenerRegistry
+ * @see EventListenerRegistryImpl
  */
 public class EventListenerRegistryCore {
 
