3673c924472310f128fe021d7cedb0866d43a769
hotire <gngh0101@gmail.com>
2022-12-13 02:19:20 +0900

    Add FastSessionServicesCore

--- src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/service/EventListenerRegistryCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/service/EventListenerRegistryCore.java
@@ -1,12 +1,19 @@
 package com.googlecode.hotire.springdatajpa.core.event.hibernate.service;
 
-import org.hibernate.event.service.spi.EventListenerRegistry;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.event.service.internal.EventListenerRegistryImpl;
+import org.hibernate.event.service.spi.EventListenerGroup;
+import org.hibernate.event.service.spi.EventListenerRegistry;
 
 /**
  * @see EventListenerRegistry
  * @see EventListenerRegistryImpl
  */
+@RequiredArgsConstructor
 public class EventListenerRegistryCore {
 
+    /**
+     * @see EventListenerRegistryImpl#registeredEventListeners
+     */
+    private final EventListenerGroup[] registeredEventListeners;
 }
---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/service/FastSessionServicesCore.java
@@ -0,0 +1,8 @@
+package com.googlecode.hotire.springdatajpa.core.event.hibernate.service;
+
+/**
+ * @see org.hibernate.internal.FastSessionServices
+ */
+public class FastSessionServicesCore {
+
+}
