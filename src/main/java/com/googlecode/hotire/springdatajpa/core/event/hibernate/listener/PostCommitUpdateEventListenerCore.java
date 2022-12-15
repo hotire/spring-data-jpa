1a3fa3eb9077f2821738d8f3411c2ecdb74fbc7c
hotire <gngh0101@gmail.com>
2022-12-16 01:21:12 +0900

    Add PostCommitUpdateEventListener

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/listener/PostCommitUpdateEventListenerCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.event.hibernate.listener;
+
+import org.hibernate.event.spi.PostCommitUpdateEventListener;
+
+/**
+ * @see PostCommitUpdateEventListener
+ */
+public class PostCommitUpdateEventListenerCore {
+
+}
