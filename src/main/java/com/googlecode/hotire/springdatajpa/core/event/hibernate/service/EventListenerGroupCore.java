2fb08c57a22f767633522d19738c61b6dc4ccb77
hotire <gngh0101@gmail.com>
2022-12-17 15:41:28 +0900

    fireEventOnEachListener

--- src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/service/EventListenerGroupCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/event/hibernate/service/EventListenerGroupCore.java
@@ -1,5 +1,11 @@
 package com.googlecode.hotire.springdatajpa.core.event.hibernate.service;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import org.hibernate.event.service.spi.EventActionWithParameter;
 import org.hibernate.event.service.spi.EventListenerGroup;
 
 /**
@@ -8,6 +14,8 @@ import org.hibernate.event.service.spi.EventListenerGroup;
  * @see org.hibernate.event.service.internal.PostCommitEventListenerGroupImpl
  */
 public class EventListenerGroupCore<T> {
+
+    private static final CompletableFuture COMPLETED = CompletableFuture.completedFuture( null );
 
     /**
      * @see org.hibernate.event.service.internal.EventListenerGroupImpl#listeners
@@ -19,5 +27,30 @@ public class EventListenerGroupCore<T> {
      * @see EventListenerGroup#appendListener(Object)
      */
     public void appendListener(T listener) {
+    }
+
+    /**
+     * @see EventListenerGroup#fireEventOnEachListener(Object, BiConsumer) 
+     */
+    public <U> void fireEventOnEachListener(final U event, final BiConsumer<T,U> actionOnEvent) {
+
+    }
+
+
+    /**
+     * @see EventListenerGroup#fireEventOnEachListener(Object, Object, EventActionWithParameter)
+     */
+    public <U,X> void fireEventOnEachListener(final U event, X param, final EventActionWithParameter<T,U,X> actionOnEvent) {
+
+    }
+
+
+    /**
+     * @see org.hibernate.event.service.internal.EventListenerGroupImpl#fireLazyEventOnEachListener(Supplier, Function)
+     */
+    public <R, U, RL> CompletionStage<R> fireLazyEventOnEachListener(
+        final Supplier<U> eventSupplier,
+        final Function<RL, Function<U, CompletionStage<R>>> fun) {
+        return COMPLETED;
     }
 }
