fe5014bde9fd97ac80663cb0f5005129d2ad237b
hotire <gngh0101@gmail.com>
2023-02-02 00:23:08 +0900

    Add log determineCurrentLookupKey

--- src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/routing/RoutingDataSource.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/routing/RoutingDataSource.java
@@ -1,12 +1,14 @@
 package com.googlecode.hotire.springdatajpa.core.data_source.routing;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
-
     @Override
     protected Object determineCurrentLookupKey() {
+        log.info("[determineCurrentLookupKey] isCurrentTransactionReadOnly: {}", TransactionSynchronizationManager.isCurrentTransactionReadOnly() );
         return TransactionSynchronizationManager.isCurrentTransactionReadOnly() ? DataSourceType.READ_ONLY : DataSourceType.READ_WRITE;
     }
 
