65155b3ca7cc227d246c59189a9b5e4cf7453bc0
hotire <gngh0101@gmail.com>
2023-02-08 02:47:00 +0900

    Add AuditedPropertiesReaderCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/envers/reader/AuditedPropertiesReaderCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.envers.reader;
+
+import org.hibernate.envers.configuration.internal.metadata.reader.AuditedPropertiesReader;
+
+/**
+ * @see AuditedPropertiesReader
+ */
+public class AuditedPropertiesReaderCore {
+
+}
