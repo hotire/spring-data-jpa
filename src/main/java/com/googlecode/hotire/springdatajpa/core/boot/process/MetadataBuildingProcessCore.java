9c254b0ea2258e143589f7e6242000cf5abd683c
hotire <gngh0101@gmail.com>
2023-02-09 03:01:15 +0900

    Add MetadataBuildingProcess

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/boot/process/MetadataBuildingProcessCore.java
@@ -0,0 +1,8 @@
+package com.googlecode.hotire.springdatajpa.core.boot.process;
+
+/**
+ * @see org.hibernate.boot.model.process.spi.MetadataBuildingProcess
+ */
+public class MetadataBuildingProcessCore {
+
+}
