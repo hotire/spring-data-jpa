415de9cc946bda341749d3e0d68383ec5fa64cb3
hotire <gngh0101@gmail.com>
2023-03-14 03:21:33 +0900

    Add DefaultMergeEventListenerCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/merge/DefaultMergeEventListenerCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.merge;
+
+import  org.hibernate.event.internal.DefaultMergeEventListener;
+
+/**
+ * @see DefaultMergeEventListener
+ */
+public class DefaultMergeEventListenerCore {
+
+}
