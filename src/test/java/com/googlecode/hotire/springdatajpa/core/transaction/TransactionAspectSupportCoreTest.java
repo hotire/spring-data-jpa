bd42440a2d6365bedc972271a9fb0ad4e257ec25
hotire <gngh0101@gmail.com>
2023-05-15 00:23:27 +0900

    printTransaction

--- src/test/java/com/googlecode/hotire/springdatajpa/core/transaction/TransactionAspectSupportCoreTest.java
+++ src/test/java/com/googlecode/hotire/springdatajpa/core/transaction/TransactionAspectSupportCoreTest.java
@@ -2,11 +2,13 @@ package com.googlecode.hotire.springdatajpa.core.transaction;
 
 import com.googlecode.hotire.springdatajpa.core.Core;
 import com.googlecode.hotire.springdatajpa.core.CoreRepository;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.NoTransactionException;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
@@ -23,31 +25,47 @@ class TransactionAspectSupportCoreTest {
 
     @Test
     void test() throws Exception {
-        log.info("no transaction, isActualTransactionActive : {}", TransactionSynchronizationManager.isActualTransactionActive());
-
         final AtomicReference<TransactionStatus> status = new AtomicReference();
         delegator.transaction(() -> {
-            log.info("transaction, isActualTransactionActive : {}", TransactionSynchronizationManager.isActualTransactionActive());
             status.set(TransactionAspectSupportCore.currentTransactionStatus());
-            print(status.get());
+            printTransaction(status.get());
             TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
                 @Override
                 public void afterCommit() {
-                    log.info("afterCommit, isActualTransactionActive : {}", TransactionSynchronizationManager.isActualTransactionActive());
-                    print(status.get());
+                    log.info("afterCommit");
+                    printTransaction(status.get());
+                    TransactionSynchronizationManager.registerSynchronization(
+                        new TransactionSynchronization() {
+                            @Override
+                            public void afterCommit() {
+                                log.info("inner afterCommit");
+                                printTransaction(status.get());
+                            }
+                        });
 //                    TransactionAspectSupportCore.currentTransactionStatus(); // throw Exception
                 }
             });
             return coreRepository.saveAndFlush(new Core());
         });
-        print(status.get());
+        printTransaction(status.get());
     }
 
-    void print(TransactionStatus status) {
+    Optional<TransactionStatus> currentTransactionStatus() {
+        try {
+            return Optional.of(TransactionAspectSupportCore.currentTransactionStatus());
+        } catch (NoTransactionException e) {
+            log.warn("[NoTransactionException] message: {}", e.getMessage(), e);
+            return Optional.empty();
+        }
+    }
+
+    void printTransaction(TransactionStatus status) {
         log.info("currentTransactionStatus : {}", status);
         log.info("isNewTransaction : {}", status.isNewTransaction());
         log.info("hasSavepoint : {}", status.hasSavepoint());
         log.info("isRollbackOnly : {}", status.isRollbackOnly());
         log.info("isCompleted : {}", status.isCompleted());
+        log.info("currentTransactionStatus : {}", currentTransactionStatus().orElse(null));
+        log.info("isActualTransactionActive : {}", TransactionSynchronizationManager.isActualTransactionActive());
     }
 }
