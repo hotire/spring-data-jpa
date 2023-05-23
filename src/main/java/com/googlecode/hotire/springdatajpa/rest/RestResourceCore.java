f12803bc14f602dca7873fdc7cd7ec180eea04ab
hotire <gngh0101@gmail.com>
2023-05-24 02:15:11 +0900

    Add RestResourceCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/RestResourceCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.rest;
+
+import org.springframework.data.rest.core.annotation.RestResource;
+
+/**
+ * @see RestResource
+ */
+public class RestResourceCore {
+
+}
