4da30ba68d7b3baa6cd037b67e1f3216f3cecd7f
hotire <gngh0101@gmail.com>
2023-05-26 02:05:51 +0900

    Add RepositoryRestMvcConfigurationCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestMvcConfigurationCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.rest.core;
+
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+/**
+ * @see RepositoryRestMvcConfiguration
+ */
+public class RepositoryRestMvcConfigurationCore {
+
+}
