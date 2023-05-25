ed0244a54d78e594b9afb359d64b3afa8ffb5ef5
hotire <gngh0101@gmail.com>
2023-05-26 02:07:22 +0900

    restHandlerMapping

--- src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestMvcConfigurationCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestMvcConfigurationCore.java
@@ -1,10 +1,24 @@
 package com.googlecode.hotire.springdatajpa.rest.core;
 
+import java.util.Optional;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryResourceMappings;
+import org.springframework.data.rest.webmvc.config.CorsConfigurationAware;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.data.rest.webmvc.support.JpaHelper;
 
 /**
  * @see RepositoryRestMvcConfiguration
  */
 public class RepositoryRestMvcConfigurationCore {
 
+
+    /**
+     * @see RepositoryRestMvcConfiguration#restHandlerMapping(Repositories, RepositoryResourceMappings, Optional, RepositoryRestConfiguration, CorsConfigurationAware)
+     */
+    public void restHandlerMapping(Repositories repositories,
+        RepositoryResourceMappings resourceMappings, Optional<JpaHelper> jpaHelper,
+        RepositoryRestConfiguration repositoryRestConfiguration, CorsConfigurationAware corsRestConfiguration) {
+    }
 }
