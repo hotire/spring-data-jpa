9b5714557b0e3d793f3c62e2c1d5d0e5ec664082
hotire <gngh0101@gmail.com>
2023-05-27 15:32:02 +0900

    RepositoryRestProperties, detectionStrategy ANNOTATED

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestPropertiesCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.rest.core;
+
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestProperties;
+
+/**
+ * @see RepositoryRestProperties
+ */
+public class RepositoryRestPropertiesCore {
+
+}
