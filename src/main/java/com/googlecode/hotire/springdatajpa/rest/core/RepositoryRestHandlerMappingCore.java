e71104b64e0cc7e66df1a48dbd4bc86ab82ec121
hotire <gngh0101@gmail.com>
2023-05-30 00:22:35 +0900

    Fix typo

--- src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestHandlerMappingCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestHandlerMappingCore.java
@@ -1,6 +1,6 @@
 package com.googlecode.hotire.springdatajpa.rest.core;
 
-import org.springframework.data.rest.webmvc.RepositoryRestHandlerMapping
+import org.springframework.data.rest.webmvc.RepositoryRestHandlerMapping;
 
 /**
  * @see RepositoryRestHandlerMapping
