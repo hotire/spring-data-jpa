a68fbce2ce572607952fc3beb77dac14ed4459af
hotire <gngh0101@gmail.com>
2023-05-31 02:29:34 +0900

    Add RepositoryRestControllerCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestControllerCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.rest.core;
+
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+/**
+ * @see RepositoryRestController
+ */
+public class RepositoryRestControllerCore {
+
+}
