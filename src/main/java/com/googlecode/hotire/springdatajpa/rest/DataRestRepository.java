8c4b2ba9f888d6a10ba7b7424465fc7ffe8b308b
hotire <gngh0101@gmail.com>
2023-06-08 08:23:09 +0900

    Add data-rest

--- src/main/java/com/googlecode/hotire/springdatajpa/rest/DataRestRepository.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/DataRestRepository.java
@@ -1,7 +1,9 @@
 package com.googlecode.hotire.springdatajpa.rest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "data-rest")
 public interface DataRestRepository extends JpaRepository<DataRest, Long> {
 
 }
