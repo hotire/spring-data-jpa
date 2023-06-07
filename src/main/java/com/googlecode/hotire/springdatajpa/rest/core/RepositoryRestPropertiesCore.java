ae3d679fa215493e94fdc758899ee893b1014f16
hotire <gngh0101@gmail.com>
2023-06-08 08:20:57 +0900

    Add detectionStrategy

--- src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestPropertiesCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryRestPropertiesCore.java
@@ -1,10 +1,16 @@
 package com.googlecode.hotire.springdatajpa.rest.core;
 
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestProperties;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy.RepositoryDetectionStrategies;
 
 /**
  * @see RepositoryRestProperties
  */
 public class RepositoryRestPropertiesCore {
 
+
+    /**
+     * @see RepositoryRestProperties#detectionStrategy
+     */
+    private RepositoryDetectionStrategies detectionStrategy = RepositoryDetectionStrategies.DEFAULT;
 }
