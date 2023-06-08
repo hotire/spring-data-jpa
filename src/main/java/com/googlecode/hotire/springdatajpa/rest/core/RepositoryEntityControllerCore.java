b5d80d29c651cff2bb085a78ccb08486076943e8
hotire <gngh0101@gmail.com>
2023-06-09 01:29:40 +0900

    Add RepositoryEntityController

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RepositoryEntityControllerCore.java
@@ -0,0 +1,8 @@
+package com.googlecode.hotire.springdatajpa.rest.core;
+
+/**
+ * @see org.springframework.data.rest.webmvc.RepositoryEntityController;
+ */
+public class RepositoryEntityControllerCore {
+
+}
