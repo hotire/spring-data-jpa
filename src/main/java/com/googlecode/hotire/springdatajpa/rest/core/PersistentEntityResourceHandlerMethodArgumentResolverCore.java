47b1fcdc6e5d00ed47bc6d5a3b6c2b6b617f43f8
hotire <gngh0101@gmail.com>
2023-06-10 13:12:13 +0900

    Add resourceInformationResolver

--- src/main/java/com/googlecode/hotire/springdatajpa/rest/core/PersistentEntityResourceHandlerMethodArgumentResolverCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/PersistentEntityResourceHandlerMethodArgumentResolverCore.java
@@ -1,10 +1,16 @@
 package com.googlecode.hotire.springdatajpa.rest.core;
 
 import org.springframework.data.rest.webmvc.config.PersistentEntityResourceHandlerMethodArgumentResolver;
+import org.springframework.data.rest.webmvc.config.RootResourceInformationHandlerMethodArgumentResolver;
 
 /**
  * @see PersistentEntityResourceHandlerMethodArgumentResolver
  */
 public class PersistentEntityResourceHandlerMethodArgumentResolverCore {
+
+    /**
+     * @see PersistentEntityResourceHandlerMethodArgumentResolver#resourceInformationResolver
+     */
+    private RootResourceInformationHandlerMethodArgumentResolver resourceInformationHandlerMethodArgumentResolver;
 
 }
---
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RootResourceInformationHandlerMethodArgumentResolverCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.rest.core;
+
+import org.springframework.data.rest.webmvc.config.RootResourceInformationHandlerMethodArgumentResolver;
+
+/**
+ * @see RootResourceInformationHandlerMethodArgumentResolver
+ */
+public class RootResourceInformationHandlerMethodArgumentResolverCore {
+
+}
