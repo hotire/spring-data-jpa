4d89cfc60c462bd603e9d222fda7d6b03dfa6511
hotire <gngh0101@gmail.com>
2023-06-10 13:13:21 +0900

    RootResourceInformationHandlerMethodArgumentResolver resolveArgument

--- src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RootResourceInformationHandlerMethodArgumentResolverCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/rest/core/RootResourceInformationHandlerMethodArgumentResolverCore.java
@@ -1,10 +1,23 @@
 package com.googlecode.hotire.springdatajpa.rest.core;
 
+import org.springframework.core.MethodParameter;
+import org.springframework.data.rest.webmvc.RootResourceInformation;
 import org.springframework.data.rest.webmvc.config.RootResourceInformationHandlerMethodArgumentResolver;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
 
 /**
  * @see RootResourceInformationHandlerMethodArgumentResolver
  */
 public class RootResourceInformationHandlerMethodArgumentResolverCore {
 
+    /**
+     * @see RootResourceInformationHandlerMethodArgumentResolver#resolveArgument(MethodParameter, ModelAndViewContainer, NativeWebRequest, WebDataBinderFactory) 
+     */
+    public RootResourceInformation resolveArgument(
+        MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return null;
+    }
 }
