22b7b55752aec2b99541566069db0a69de9a666c
hotire <gngh0101@gmail.com>
2023-06-12 01:08:05 +0900

    Add RepositoryInvokerFactoryCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/RepositoryInvokerFactoryCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.repository;
+
+import org.springframework.data.repository.support.RepositoryInvokerFactory;
+
+/**
+ * @see RepositoryInvokerFactory
+ */
+public class RepositoryInvokerFactoryCore {
+
+}
