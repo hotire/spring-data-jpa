c8003cb082dfca74631e2fdcadf2edfed55ebf89
hotire <gngh0101@gmail.com>
2023-06-12 01:09:11 +0900

    getInvokerFor in RepositoryInvokerFactory

--- src/main/java/com/googlecode/hotire/springdatajpa/core/repository/RepositoryInvokerFactoryCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/RepositoryInvokerFactoryCore.java
@@ -1,10 +1,18 @@
 package com.googlecode.hotire.springdatajpa.core.repository;
 
+import org.springframework.data.repository.support.RepositoryInvoker;
 import org.springframework.data.repository.support.RepositoryInvokerFactory;
+import org.springframework.data.repository.support.DefaultRepositoryInvokerFactory;
 
 /**
  * @see RepositoryInvokerFactory
+ * @see DefaultRepositoryInvokerFactory
  */
-public class RepositoryInvokerFactoryCore {
+public interface RepositoryInvokerFactoryCore {
 
+
+    /**
+     * @see RepositoryInvokerFactory#getInvokerFor(Class) 
+     */
+    RepositoryInvoker getInvokerFor(Class<?> domainType);
 }
