9e07b31f23104e04020de0c2ccdb34897f4de111
hotire <gngh0101@gmail.com>
2023-08-07 00:07:00 +0900

    Add QueryExecutorMethodInterceptor.queries

--- src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/QueryExecutorMethodInterceptorCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/QueryExecutorMethodInterceptorCore.java
@@ -1,6 +1,8 @@
 package com.googlecode.hotire.springdatajpa.core.repository.method;
 
 import java.lang.reflect.Method;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.query.QueryLookupStrategy;
@@ -10,7 +12,13 @@ import org.springframework.data.util.Pair;
 /**
  * @see org.springframework.data.repository.core.support.QueryExecutorMethodInterceptor
  */
+@RequiredArgsConstructor
 public class QueryExecutorMethodInterceptorCore {
+
+    /**
+     * @see org.springframework.data.repository.core.support.QueryExecutorMethodInterceptor#queries
+     */
+    private final Map<Method, RepositoryQuery> queries;
 
 
     /**
