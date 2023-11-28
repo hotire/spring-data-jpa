b83016e0fa6b608584c5c2475a2d2d232cd614e3
hotire <gngh0101@gmail.com>
2023-11-29 01:22:29 +0900

    Add TupleBackedMapCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/projection/TupleBackedMapCore.java
@@ -0,0 +1,11 @@
+package com.googlecode.hotire.springdatajpa.core.projection;
+
+import org.springframework.data.jpa.repository.query.AbstractJpaQuery;
+
+/**
+ * @see AbstractJpaQuery.TupleConverter
+ * @see AbstractJpaQuery.TupleConverter.TupleBackedMap
+ */
+public class TupleBackedMapCore {
+
+}
