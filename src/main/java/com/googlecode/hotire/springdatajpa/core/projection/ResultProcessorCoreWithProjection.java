24bb4f669dab9dd73d7dd65adc618f2af94c1ac3
hotire <gngh0101@gmail.com>
2023-12-04 01:54:24 +0900

    ResultProcessorCoreWithProjection

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/projection/ResultProcessorCoreWithProjection.java
@@ -0,0 +1,20 @@
+package com.googlecode.hotire.springdatajpa.core.projection;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.repository.query.ResultProcessor;
+import org.springframework.lang.Nullable;
+
+/**
+ * @see ResultProcessor
+ */
+public class ResultProcessorCoreWithProjection {
+
+
+    /**
+     * @see ResultProcessor#processResult(Object, Converter) 
+     */
+    public <T> T processResult(@Nullable Object source, Converter<Object, Object> preparingConverter) {
+        return null;
+    }
+
+}
