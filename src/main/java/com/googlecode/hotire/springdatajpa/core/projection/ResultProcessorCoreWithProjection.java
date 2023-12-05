3606ceb9cd6b8d96037d0f02b92d61b158fa8727
hotire <gngh0101@gmail.com>
2023-12-06 03:17:20 +0900

    ResultProcessor.ProjectingConverter#convert(Object)

--- src/main/java/com/googlecode/hotire/springdatajpa/core/projection/ResultProcessorCoreWithProjection.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/projection/ResultProcessorCoreWithProjection.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.repository.query.ResultProcessor;
+import org.springframework.data.repository.query.ReturnedType;
 import org.springframework.lang.Nullable;
 
 /**
@@ -29,12 +30,18 @@ public class ResultProcessorCoreWithProjection {
      */
     @RequiredArgsConstructor
     static class ProjectingConverter {
-
+        private final ReturnedType type;
         /**
          * @see org.springframework.data.projection.ProjectionFactory;
          */
         private final ProjectionFactory factory;
 
+        /**
+         * @see ResultProcessor.ProjectingConverter#convert(Object)
+         */
+        public Object convert(Object source) {
+            return null;
+        }
     }
 
 }
