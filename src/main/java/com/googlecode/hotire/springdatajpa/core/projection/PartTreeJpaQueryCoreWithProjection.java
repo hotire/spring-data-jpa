c85a1f14f78caba5c5e9033490c9ede5f4da535c
hotire <gngh0101@gmail.com>
2023-12-07 02:35:15 +0900

    Add PartTreeJpaQueryCoreWithProjection

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/projection/PartTreeJpaQueryCoreWithProjection.java
@@ -0,0 +1,18 @@
+package com.googlecode.hotire.springdatajpa.core.projection;
+
+import org.springframework.data.jpa.repository.query.PartTreeJpaQuery;
+
+/**
+ * @see PartTreeJpaQuery
+ */
+public class PartTreeJpaQueryCoreWithProjection {
+
+
+    /**
+     * @see PartTreeJpaQuery.QueryPreparer
+     */
+    private class QueryPreparer {
+
+    }
+
+}
