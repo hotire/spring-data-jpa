a1bd7871629ce7cad0ddab21ac4cbb1c94fda3bb
hotire <gngh0101@gmail.com>
2023-12-07 02:34:13 +0900

    Add CriteriaQueryTupleTransformerCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/projection/CriteriaQueryTupleTransformerCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.projection;
+
+import org.hibernate.jpa.spi.CriteriaQueryTupleTransformer;
+
+/**
+ * @see CriteriaQueryTupleTransformer
+ */
+public class CriteriaQueryTupleTransformerCore {
+
+}
