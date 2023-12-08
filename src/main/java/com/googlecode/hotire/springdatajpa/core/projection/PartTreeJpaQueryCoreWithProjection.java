850b00796e5dec29147c4cad469292af762cbe3a
hotire <gngh0101@gmail.com>
2023-12-08 21:00:20 +0900

    Add PartTreeJpaQuery.QueryPreparer#createCreator(JpaParametersParameterAccessor)

--- src/main/java/com/googlecode/hotire/springdatajpa/core/projection/PartTreeJpaQueryCoreWithProjection.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/projection/PartTreeJpaQueryCoreWithProjection.java
@@ -1,5 +1,7 @@
 package com.googlecode.hotire.springdatajpa.core.projection;
 
+import javax.persistence.Query;
+import org.springframework.data.jpa.repository.query.JpaParametersParameterAccessor;
 import org.springframework.data.jpa.repository.query.PartTreeJpaQuery;
 
 /**
@@ -13,6 +15,13 @@ public class PartTreeJpaQueryCoreWithProjection {
      */
     private class QueryPreparer {
 
+
+        /**
+         * @see PartTreeJpaQuery.QueryPreparer#createCreator(JpaParametersParameterAccessor) 
+         */
+        public Query createQuery(JpaParametersParameterAccessor accessor) {
+            return null;
+        }
     }
 
 }
