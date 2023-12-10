4066160f5952eeb75b20374715c9951e76e82d2f
hotire <gngh0101@gmail.com>
2023-12-11 01:28:03 +0900

    Add SingleEntityExecutionCoreWithProjection

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/projection/SingleEntityExecutionCoreWithProjection.java
@@ -0,0 +1,8 @@
+package com.googlecode.hotire.springdatajpa.core.projection;
+
+/**
+ * @see org.springframework.data.jpa.repository.query.JpaQueryExecution.SingleEntityExecution;
+ */
+public class SingleEntityExecutionCoreWithProjection {
+
+}
