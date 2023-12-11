e6e8251107713d3f152750ccb53934a40bf6610f
hotire <gngh0101@gmail.com>
2023-12-12 01:08:10 +0900

    Add AbstractJpaQuery#execution

--- src/main/java/com/googlecode/hotire/springdatajpa/core/projection/AbstractJpaQueryCoreWithProjection.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/projection/AbstractJpaQueryCoreWithProjection.java
@@ -1,12 +1,20 @@
 package com.googlecode.hotire.springdatajpa.core.projection;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.query.AbstractJpaQuery;
 import org.springframework.data.jpa.repository.query.JpaQueryExecution;
+import org.springframework.data.util.Lazy;
 
 /**
  * @see AbstractJpaQuery
  */
+@RequiredArgsConstructor
 public class AbstractJpaQueryCoreWithProjection {
+
+    /**
+     * @see AbstractJpaQuery#execution
+     */
+    private final Lazy<JpaQueryExecution> execution;
 
 
     /**
