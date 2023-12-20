e91414d3264f5ebeb547e03141798f69c1de7974
hotire <gngh0101@gmail.com>
2023-12-21 01:18:47 +0900

    Add DeferredRepositoryInitializationListenerCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/bootstrap/DeferredRepositoryInitializationListenerCore.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.springdatajpa.core.bootstrap;
+
+
+/**
+ * @see org.springframework.data.repository.config.DeferredRepositoryInitializationListener
+ */
+public class DeferredRepositoryInitializationListenerCore {
+
+}
