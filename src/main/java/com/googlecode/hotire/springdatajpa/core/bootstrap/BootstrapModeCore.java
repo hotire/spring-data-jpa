f1d7fe4ff4c8346f48bdf9ddae0442069918e864
hotire <gngh0101@gmail.com>
2023-12-20 22:07:33 +0900

    Add BootstrapModeCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/bootstrap/BootstrapModeCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.bootstrap;
+
+import org.springframework.data.repository.config.BootstrapMode;
+
+/**
+ * @see BootstrapMode
+ */
+public class BootstrapModeCore {
+
+}
