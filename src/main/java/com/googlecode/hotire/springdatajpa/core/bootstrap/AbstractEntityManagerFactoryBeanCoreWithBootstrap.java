0e8d96673aafc616ddaf2990117c9570ac644752
hotire <gngh0101@gmail.com>
2023-12-22 01:05:16 +0900

    Add AbstractEntityManagerFactoryBean#buildNativeEntityManagerFactory()

--- src/main/java/com/googlecode/hotire/springdatajpa/core/bootstrap/AbstractEntityManagerFactoryBeanCoreWithBootstrap.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/bootstrap/AbstractEntityManagerFactoryBeanCoreWithBootstrap.java
@@ -1,5 +1,6 @@
 package com.googlecode.hotire.springdatajpa.core.bootstrap;
 
+import javax.persistence.EntityManagerFactory;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
 
@@ -13,4 +14,11 @@ public class AbstractEntityManagerFactoryBeanCoreWithBootstrap {
      */
     private AsyncTaskExecutor bootstrapExecutor;
 
+
+    /**
+     * @see AbstractEntityManagerFactoryBean#buildNativeEntityManagerFactory()
+     */
+    private EntityManagerFactory buildNativeEntityManagerFactory() {
+        return null;
+    }
 }
