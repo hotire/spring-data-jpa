8866fcd945153ee73407e63d0b5a066e7b2ec006
hotire <gngh0101@gmail.com>
2023-12-22 01:06:30 +0900

    Add onApplicationEvent

--- src/main/java/com/googlecode/hotire/springdatajpa/core/bootstrap/DeferredRepositoryInitializationListenerCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/bootstrap/DeferredRepositoryInitializationListenerCore.java
@@ -1,9 +1,31 @@
 package com.googlecode.hotire.springdatajpa.core.bootstrap;
 
 
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.data.repository.Repository;
+
 /**
  * @see org.springframework.data.repository.config.DeferredRepositoryInitializationListener
  */
+@RequiredArgsConstructor
 public class DeferredRepositoryInitializationListenerCore {
+    private static final Log logger = LogFactory.getLog(DeferredRepositoryInitializationListenerCore.class);
+    private final ListableBeanFactory beanFactory;
+
+    /**
+     * @see org.springframework.data.repository.config.DeferredRepositoryInitializationListener#onApplicationEvent(ContextRefreshedEvent) 
+     */
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        logger.info("Triggering deferred initialization of Spring Data repositories…");
+
+        beanFactory.getBeansOfType(Repository.class);
+
+        logger.info("Spring Data repositories initialized!");
+    }
 
 }
