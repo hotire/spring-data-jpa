71fc097eb03f4dd9a0cf0c994104f1a295451f3b
hotire <gngh0101@gmail.com>
2024-01-02 19:59:55 +0900

    Add RepositoryConfigurationDelegateCoreWithBootstrap

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/bootstrap/RepositoryConfigurationDelegateCoreWithBootstrap.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.bootstrap;
+
+import org.springframework.data.repository.config.RepositoryConfigurationDelegate;
+
+/**
+ * @see RepositoryConfigurationDelegate
+ */
+public class RepositoryConfigurationDelegateCoreWithBootstrap {
+
+}
