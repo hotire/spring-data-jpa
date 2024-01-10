4edfe2457ef8dbbdb9ac58f28b98a01255f60ba4
hotire <gngh0101@gmail.com>
2024-01-11 01:38:57 +0900

    Add HikariCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/hikari_cp/HikariCore.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.springdatajpa.hikari_cp;
+
+/**
+ * data source
+ * @see com.googlecode.hotire.springdatajpa.core.data_source.hikari
+ */
+public class HikariCore {
+
+}
