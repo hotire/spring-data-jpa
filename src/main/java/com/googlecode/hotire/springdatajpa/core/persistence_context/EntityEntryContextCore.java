33c04ed65a52894ea17e6c1637c1735d98549f2d
hotire <gngh0101@gmail.com>
2024-02-05 01:54:25 +0900

    Add EntityEntryContext

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/persistence_context/EntityEntryContextCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.persistence_context;
+
+import org.hibernate.engine.internal.EntityEntryContext;
+
+/**
+ * @see EntityEntryContext
+ */
+public class EntityEntryContextCore {
+
+}
