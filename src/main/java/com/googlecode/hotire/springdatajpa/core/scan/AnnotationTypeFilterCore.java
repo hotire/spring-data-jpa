467bb8a400812dc856ff0aefd38a7225e9f01e0d
hotire <gngh0101@gmail.com>
2024-03-14 02:45:03 +0900

    Add AnnotationTypeFilterCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/scan/AnnotationTypeFilterCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.scan;
+
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+
+/**
+ * @see AnnotationTypeFilter
+ */
+public class AnnotationTypeFilterCore {
+
+}
