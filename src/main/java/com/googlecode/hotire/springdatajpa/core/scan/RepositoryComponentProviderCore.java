83516eccf56d4332579ae25d5fdbb9262cdcb242
hotire <gngh0101@gmail.com>
2024-03-15 01:26:37 +0900

    Add RepositoryComponentProviderCore

--- src/main/java/com/googlecode/hotire/springdatajpa/core/scan/RepositoryComponentProviderCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/scan/RepositoryComponentProviderCore.java
@@ -1,8 +1,26 @@
 package com.googlecode.hotire.springdatajpa.core.scan;
 
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.data.repository.util.ClassUtils;
+
 /**
  * @see org.springframework.data.repository.config.RepositoryComponentProvider
  */
 public class RepositoryComponentProviderCore {
+
+
+    /**
+     * @see org.springframework.data.repository.config.RepositoryComponentProvider#isCandidateComponent(MetadataReader) 
+     */
+    protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
+
+        boolean isNonRepositoryInterface = !ClassUtils.isGenericRepositoryInterface(beanDefinition.getBeanClassName());
+        boolean isTopLevelType = !beanDefinition.getMetadata().hasEnclosingClass();
+//        boolean isConsiderNestedRepositories = isConsiderNestedRepositoryInterfaces();
+        boolean isConsiderNestedRepositories = true;
+
+        return isNonRepositoryInterface && (isTopLevelType || isConsiderNestedRepositories);
+    }
 
 }
