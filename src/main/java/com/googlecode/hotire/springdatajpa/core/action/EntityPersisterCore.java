da283335a452ae04140fa6070c4b807e59e77769
hotire <gngh0101@gmail.com>
2025-06-08 21:00:42 +0900

    EntityPersister#insert(Object[], Object, SharedSessionContractImplementor)

--- src/main/java/com/googlecode/hotire/springdatajpa/core/action/EntityPersisterCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/action/EntityPersisterCore.java
@@ -9,7 +9,7 @@ import org.hibernate.engine.spi.SharedSessionContractImplementor;
 public class EntityPersisterCore {
 
     /**
-     * @see org.hibernate.persister.entity.EntityPersister#insert(Object[], Object, SharedSessionContractImplementor) 
+     * @see org.hibernate.persister.entity.EntityPersister#insert(Object[], Object, SharedSessionContractImplementor)
      */
     public void insert(Serializable id, Object[] fields, Object object, SharedSessionContractImplementor session) {
         
