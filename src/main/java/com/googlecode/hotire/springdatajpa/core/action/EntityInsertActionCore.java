1c929536e8a31caf1981fb2980ebba20c4634ae2
hotire <gngh0101@gmail.com>
2025-06-08 20:56:21 +0900

    EntityInsertAction#execute()

--- src/main/java/com/googlecode/hotire/springdatajpa/core/action/EntityInsertActionCore.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/action/EntityInsertActionCore.java
@@ -1,8 +1,18 @@
 package com.googlecode.hotire.springdatajpa.core.action;
 
+import org.hibernate.HibernateException;
+import org.hibernate.action.internal.EntityInsertAction;
+
 /**
  * @see org.hibernate.action.internal.EntityInsertAction
  */
 public class EntityInsertActionCore {
 
+    /**
+     * @see EntityInsertAction#execute()
+     * @throws HibernateException
+     */
+    public void execute() throws HibernateException {
+
+    }
 }
